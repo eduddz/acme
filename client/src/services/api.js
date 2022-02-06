d890f22604f6979df3150abce4c25fdeed09a108
eduddz <esteves-dorta@hotmail.com>
2022-02-06 18:25:29 -0300

    Correção de segurança e performance

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,5 @@
-require('dotenv').config();
 import axios from "axios";
 
-const port = process.env.PORT_SERVER || 8080;
-
 export const api = axios.create({
-    baseURL: `http://localhost:${port}/pacientes/`
+    baseURL: "http://localhost:3001/pacientes/"
 });
